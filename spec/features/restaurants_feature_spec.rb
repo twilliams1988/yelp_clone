a038ca04459fab10f72bb0b6f945acb5a6778e06
Rylan Gooch <rylangooch@gmail.com>
2016-08-22 16:42:00 +0100

    Added restaurant view functionality

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -25,6 +25,16 @@ feature 'restaurants' do
     end
   end
 
+  context 'viewing restaurants' do
+    let!(:kfc){ Restaurant.create(name: 'KFC') }
+
+    scenario 'lets a user view a restaurant' do
+      visit '/restaurants'
+      click_link 'KFC'
+      expect(page).to have_content 'KFC'
+      expect(current_path).to eq "/restaurants/#{kfc.id}"
+    end
+  end
 
   context 'no restaurants should have been added' do
     scenario 'should display a promt to add a restaurant' do
