ae662554f875d1f10fb8699c2ff88cd776a893f8
sus111 <sgraham535@gmail.com>
2016-08-22 17:06:39 +0100

    new restaurant form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,20 @@
 class RestaurantsController < ApplicationController
 
   def index
-    # raise "Hello from the index action"
+    @restaurants = Restaurant.all
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
+
 end
