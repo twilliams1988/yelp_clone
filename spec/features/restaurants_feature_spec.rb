8596f48e98d13df9593f5f36ebb00cde6c0b17e9
lukew244 <luke.webster86@gmail.com>
2016-08-22 15:44:26 +0100

    Adds first restaurant

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,6 +1,19 @@
 require 'rails_helper'
 
 feature 'restaurants' do
+  context 'restaurants have been added' do
+    before do
+      Restaurant.create(name: 'KFC')
+    end
+
+    scenario 'display restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content 'KFC'
+      expect(page).not_to have_content 'No restaurants yet'
+    end
+  end
+
+
   context 'no restaurants should have been added' do
     scenario 'should display a promt to add a restaurant' do
       visit '/restaurants'
