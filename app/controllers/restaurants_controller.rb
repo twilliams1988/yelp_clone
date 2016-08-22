1fc8dc1f3b1e5350542c2dad6fa94205a76386c8
Jason Luong <jasonluong@Jasons-MacBook-Pro.local>
2016-08-22 16:28:00 +0100

    completed step 9

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,12 +2,18 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
   def new
     @restaurant = Restaurant.new
   end
+
   def create
     @restaurant = Restaurant.create(restaurant_params)
     redirect_to '/restaurants'
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
   private
