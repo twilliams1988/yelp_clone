a3a23145051ff23378979314b7d698ce9dcb2429
lukew244 <luke.webster86@gmail.com>
2016-08-23 10:59:38 +0100

    Adds reviews

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -37,6 +37,6 @@ class RestaurantsController < ApplicationController
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name)
+    params.require(:restaurant).permit(:name, :description)
   end
 end
