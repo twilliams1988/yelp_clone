c67ca806a17a82175ecbb61b7008fc44debb69f3
sus111 <sgraham535@gmail.com>
2016-08-23 14:53:04 +0100

    user sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :restaurants do
   resources :reviews
 end
+
+root to: "restaurants#index"
 
 end
