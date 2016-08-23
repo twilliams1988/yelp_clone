08b5a04714b957d36b5cc65f682b7b41a65e621d
sus111 <sgraham535@gmail.com>
2016-08-23 14:27:06 +0100

    validate review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,4 @@
 class Review < ApplicationRecord
+
+  validates :rating, inclusion: (1..5)
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -6,4 +6,10 @@ describe Restaurant, type: :model do
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
   end
+
+  it 'is not valid unless it has a unique name' do
+    Restaurant.create(name: "Whatever you want Bar")
+    restaurant = Restaurant.new(name: "Whatever you want Bar")
+    expect(restaurant).to have(1).error_on(:name)
+  end
 end
