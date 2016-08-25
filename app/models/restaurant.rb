cba71a83b00c8b928c488fb2de83e9e76bfb27b4
Jason Luong <jasonluong@Jasons-MacBook-Pro.local>
2016-08-25 15:19:05 +0100

    created models for 1 review per restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,6 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
   belongs_to :user
-
   has_many :reviews, dependent: :destroy
+
   validates :name, length: { minimum: 3 }, uniqueness: true
 end
